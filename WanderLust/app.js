349d9f3408f1b80fd6e67378e4cf549beced0f8f
Kratika Jain <jkratika8@gmail.com>
2025-08-31 20:18:36 +0530

    done

--- WanderLust/app.js
+++ WanderLust/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
 
@@ -16,24 +18,90 @@ main()
 async function main() {
 await mongoose.connect(MONGO_URL);
 }
-
+ app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 app.get("/", (req,res)=> {
     res.send("Hii, I am root.");
 });
 
-app.get("/testListing", (req,res)=> {
- let sampleListing = new Listing ({
-    title: "My New Villa",
-    descrition: "By the beach",
-    price: 1200,
-    location: "Calangute, Goa",
-    country: "India"
- });
-});
+// app.get("/testListing", async(req,res)=> {
+//     try {
+//   let sampleListing = new Listing ({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India"
+    
+
+//  });
+//  await sampleListing.save();
+//  console.log("Sample was saved.");
+//  res.send("successful");
+// } catch (err) {
+//     console.log("Error while saving:", err);
+//     res.status(500).send("Error saving listing");
+//   }
+// });
+
+//Index Route
+app.get("/listings", async (req, res)=> {
+      const allListings = await Listing.find({});
+      res.render("listings/index", {allListings});
+    });
+
+
+     //New Route
+    app.get("/listings/new", (req, res)=> {
+    res.render("listings/new.ejs");
+    });
+
+    //Show Route
+    app.get("/listings/:id", async(req, res)=> {
+        let {id} = req.params;
+        const listing = await Listing.findById(id);
+        res.render("listings/show", {listing});
+    });
+
+    //Create Route
+    app.post("/listings", async (req, res)=> {
+        // let {title, description, image, price, country, location } = req.body;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+    
+    
+    });
+
+    //Edit Route
+    app.get("/listings/:id/edit", async(req,res)=> {
+        let {id} = req.params;
+        const listing = await Listing.findById(id);
+        res.render("listings/edit.ejs", {listing});
+    })
+
+  //Update Route
+  app.put("/listings/:id", async (req, res)=> {
+    let {id} = req.params;
+     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+     res.redirect(`/listings/${id}`);
+  })
+
+  //Delete route
+  app.delete("/listings/:id", async (req,res)=> {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+ console.log(deletedListing);
+ res.redirect("/listings");
+  })
+   
 
 
 app.listen(8080, ()=> {
     console.log("Server is listening to port 8080.");
-});
+}); 
+
--- WanderLust/models/listing.js
+++ WanderLust/models/listing.js
@@ -4,15 +4,29 @@ const Schema = mongoose.Schema;
 const listingSchema = new Schema({
     title: {
    type: String,
-   require: true
+   required: true
     } ,
     
 
     description: String,
-    image:{
-   type: String,
-   set: (v)=> v=== "" ? "https://www.wallartprints.com.au/blog/nature-pictures/" 
-   : v,
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: "https://www.wallartprints.com.au/blog/nature-pictures/",
+            set: (v) => v === "" ? "https://www.wallartprints.com.au/blog/nature-pictures/" : v
+        }
+//     image:{
+//         filename: String,
+//         url: String
+   
+// //    default:
+// //     "https://www.wallartprints.com.au/blog/nature-pictures/" ,
+//    set: (v)=> v=== "" ? "https://www.wallartprints.com.au/blog/nature-pictures/" 
+//    : v,
     },
     
     price: Number,
@@ -21,4 +35,4 @@ const listingSchema = new Schema({
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-module.export = Listing;
+module.exports = Listing;

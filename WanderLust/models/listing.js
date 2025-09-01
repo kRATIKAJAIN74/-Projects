8bca1db9d0e31e3ee606c55ca1ca8e202c5817ac
Kratika Jain <jkratika8@gmail.com>
2025-09-01 20:22:46 +0530

    done

--- WanderLust/models/listing.js
+++ WanderLust/models/listing.js
@@ -2,36 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-   type: String,
-   required: true
-    } ,
-    
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+  filename: String,
+  url: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?...",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?..."
+        : v,
+  }
+},
 
-    description: String,
-    image: {
-        filename: {
-            type: String,
-            default: "listingimage"
-        },
-        url: {
-            type: String,
-            default: "https://www.wallartprints.com.au/blog/nature-pictures/",
-            set: (v) => v === "" ? "https://www.wallartprints.com.au/blog/nature-pictures/" : v
-        }
-//     image:{
-//         filename: String,
-//         url: String
-   
-// //    default:
-// //     "https://www.wallartprints.com.au/blog/nature-pictures/" ,
-//    set: (v)=> v=== "" ? "https://www.wallartprints.com.au/blog/nature-pictures/" 
-//    : v,
-    },
-    
-    price: Number,
-    location: String,
-    country: String,
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
